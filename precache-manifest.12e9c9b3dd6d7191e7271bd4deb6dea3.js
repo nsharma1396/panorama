d0051049bc8f4bcd3b1feccb4b6d4d240e1e3fe2
Neeraj <n.sharma1396@gmail.com>
2020-03-13 00:57:15 +0530

    Updates

---
+++ precache-manifest.12e9c9b3dd6d7191e7271bd4deb6dea3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e27cf87eec2a618524a373a7c6f7c2",
+    "url": "/panorama/index.html"
+  },
+  {
+    "revision": "be2527b9e6b4fe4247ca",
+    "url": "/panorama/static/css/main.b6f51580.chunk.css"
+  },
+  {
+    "revision": "147aa4e7131ee69a72ca",
+    "url": "/panorama/static/js/2.40753c34.chunk.js"
+  },
+  {
+    "revision": "9b9aea2a2c43fbdb6ac13feed4504f2c",
+    "url": "/panorama/static/js/2.40753c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2527b9e6b4fe4247ca",
+    "url": "/panorama/static/js/main.0042f2c1.chunk.js"
+  },
+  {
+    "revision": "6c9a05a75e2b358969a7",
+    "url": "/panorama/static/js/runtime-main.c5c44b1e.js"
+  },
+  {
+    "revision": "e42882882e5eaa79bc8266c2f988739c",
+    "url": "/panorama/static/media/Click2.e4288288.mp3"
+  }
+]);
